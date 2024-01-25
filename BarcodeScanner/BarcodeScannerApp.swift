af80a99f67ba4016dbcda4ccf5d420d0ade979db
rick2785 <rjhrabowskie@gmail.com>
2024-01-25 10:05:43 -0600

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by RJ Hrabowskie on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
