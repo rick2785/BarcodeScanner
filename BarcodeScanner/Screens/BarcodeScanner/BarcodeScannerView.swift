2ab0e84ac20606a9a0e3927990bbc49d5374a79b
rick2785 <rjhrabowskie@gmail.com>
2024-01-29 18:35:21 -0600

    first commit

---
+++ BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by RJ Hrabowskie on 1/25/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    
+    @StateObject var viewModel = BarcodeScannerViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScannerView(scannedCode: $viewModel.scannedCode, alertItem: $viewModel.alertItem)
+                    .frame(maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text(viewModel.statusText)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(viewModel.statusTextColor)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+            }
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
